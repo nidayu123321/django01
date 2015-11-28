c2f29b450e1ff104c5e3fd76a2496b0c1d03e574
nidayu <18317042860@163.com>
2015-11-28 21:56:46 +0800

    页面标签的使用方法

--- blog/views.py
+++ blog/views.py
@@ -29,9 +29,15 @@ class Person(object):
 def index(req):
     user = Person('倪达玉', 24, '男')
     book_list = ['python', 'java', 'php', 'C++']
+    zidian = {
+        'name': 'tom',
+        'age': '22',
+        'school': 'ldu'
+    }
     # 传递参数
     return render_to_response('index.html', {
         'title': 'django_test',
         'user': user,
-        'book_list': book_list
+        'book_list': book_list,
+        'zidian': zidian
     })
