ac2400704ab28eb353419e4e0b356a7437d3d369
nidayu <18317042860@163.com>
2015-11-28 20:05:01 +0800

    传递参赛

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 from django.http import HttpResponse
 from django.template import Context, loader
 from django.shortcuts import render_to_response
@@ -12,6 +14,24 @@ def index(req):
     # return HttpResponse('<h1>welcome to django!</h1>')
 '''
 
+
+class Person(object):
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
+    def say(self):
+        return 'my name is ' + self.name
+
+
 # 第二种方法
-def index():
-    return render_to_response('index.html', {})
+def index(req):
+    user = Person('倪达玉', 24, '男')
+    book_list = ['python', 'java', 'php', 'C++']
+    # 传递参数
+    return render_to_response('index.html', {
+        'title': 'django_test',
+        'user': user,
+        'book_list': book_list
+    })
